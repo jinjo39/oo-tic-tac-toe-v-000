64831397d3e4581b4c81521681e6dcdf37e876d1
Christina Delavar <c.delavar@gmail.com>
2018-09-11 21:04:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -66,4 +66,16 @@ end
   def turn_count
     @board.count{|token| token == "X" || token == "O"}
   end
+
+  def won?
+    indexes = Array.new
+    if WIN_COMBINATIONS.any? do |combination|
+      indexes = combination
+      (@board[combination[0]] == "X" && @board[combination[1]] == "X" && @board[combination[2]] == "X") || (@board[combination[0]] == "O" && @board[combination[1]] == "O" && @board[combination[2]] == "O")
+    end
+      return indexes
+    else
+      return false
+    end
+  end
 end
